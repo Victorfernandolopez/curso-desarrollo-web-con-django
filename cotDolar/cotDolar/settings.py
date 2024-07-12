4e906e1e5621283d9d66da0991ae749381293de6
Lopez Victor Fernando <victorfernandolopez1988@gmail.com>
2024-07-12 18:32:31 -0300

    clase 3

--- cotDolar/cotDolar/settings.py
+++ cotDolar/cotDolar/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cotDolarApp.apps.cotDolarappConfig'
+    'cotDolarApp.apps.CotDolarappConfig'
 ]
 
 MIDDLEWARE = [
--- cotDolar/cotDolarApp/views.py
+++ cotDolar/cotDolarApp/views.py
@@ -1,6 +1,27 @@
-
-import requests
-from django.http import HttpResponseRedirect
-
+import sqlite3
+from django.http import HttpResponseRedirect,HttpResponse, request
+from django.shortcuts import render
 def index(request):
-    return HttpResponseRedirect("https://api.recursospython.com/dollar")
+    ctx = {"nombre" : "juan"}
+    return render(request, "cotDolarApp/index.html",ctx)
+
+def cotizacion_dolar(request):
+    r = request.get("https://dolarapi.com/v1/dolares/blue")
+    resultado = r.json()
+    html = f"""
+        <html>
+        <title>Cotizacion del dolar</title>
+        <p><strong>Compra</strong>: {resultado["Compra"]}</p>
+        <p><strong>Venta</strong>: {resultado["Venta"]}</p>
+        </html>
+        """
+    return HttpResponse(html)
+
+def cursos (request):
+    conn = sqlite3.connect("db.sqlite3")
+    cursor = conn.cursor()
+    cursor.execute("SELECT nombre, inscriptos FROM cursos")
+    cursos = cursor.fetchall()
+    conn.close()
+    ctx = {"cursos": cursos}
+    return render(request,"cotDolarApp/cursos.html", ctx)
